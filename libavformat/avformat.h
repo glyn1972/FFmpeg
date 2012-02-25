0400c96ac2535eba0a5d8239f1a3b5d8d6a3d111
Hendrik Leppkes <h.leppkes@gmail.com>
2012-02-25 23:29:38 +0100

    avformat: add a new flag to let the user indicate the file is on a slow network device

--- libavformat/avformat.h
+++ libavformat/avformat.h
@@ -1475,6 +1475,8 @@ typedef struct AVFormatContext {
 #endif
 #define AVFMT_FLAG_AUTO_BSF   0x200000 ///< Add bitstream filters as requested by the muxer
 
+#define AVFMT_FLAG_NETWORK        0x80000000 ///< Source is a network protocol, optimize for that
+
     /**
      * Maximum number of bytes read from input in order to determine stream
      * properties. Used when reading the global header and in
