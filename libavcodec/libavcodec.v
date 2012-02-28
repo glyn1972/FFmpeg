9cd2ef85c3af951a1e77d8eb4838afdd61f2a049
Hendrik Leppkes <h.leppkes@gmail.com>
2012-02-28 20:07:30 +0100

    Export avcodec functions used in LAV

--- libavcodec/libavcodec.v
+++ libavcodec/libavcodec.v
@@ -4,6 +4,10 @@ LIBAVCODEC_MAJOR {
         avcodec_*;
         avpriv_*;
         avsubtitle_free;
+        #LAV usage
+        ff_vc1_pixel_aspect;
+        ff_crop_tab;
+        ff_flac_is_extradata_valid;
     local:
         *;
 };
