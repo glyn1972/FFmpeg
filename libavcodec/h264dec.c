1fae0a517eab5b726c4ccb892eda5cc5af785c03
Hendrik Leppkes <h.leppkes@gmail.com>
2013-05-11 23:19:59 +0200

    h264: set h->sps if none is set yet

--- libavcodec/h264dec.c
+++ libavcodec/h264dec.c
@@ -418,6 +418,17 @@ FF_ENABLE_DEPRECATION_WARNINGS
                ret = 0;
            }
         }
+
+        /* activate the first SPS to determine basic stream information */
+        if (!h->ps.sps) {
+            int i;
+            for (i = 0; i < FF_ARRAY_ELEMS(h->ps.pps_list) && !h->ps.sps; i++) {
+                if (h->ps.pps_list[i]) {
+                    av_refstruct_replace(&h->ps.pps, h->ps.pps_list[i]);
+                    h->ps.sps = h->ps.pps->sps;
+                }
+            }
+        }
     }
 
     if (h->ps.sps) {
