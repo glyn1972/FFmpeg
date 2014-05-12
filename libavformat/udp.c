7d8b187215131efb0ee1e82b354bdef01c93fe70
Hendrik Leppkes <h.leppkes@gmail.com>
2014-05-12 08:38:33 +0200

    HACK: udp: allow circular receive buffer with w32threads

--- libavformat/udp.c
+++ libavformat/udp.c
@@ -1118,6 +1118,7 @@ static int udp_close(URLContext *h)
     if (s->is_multicast && (h->flags & AVIO_FLAG_READ))
         udp_leave_multicast_group(s->udp_fd, (struct sockaddr *)&s->dest_addr,
                                   (struct sockaddr *)&s->local_addr_storage, h);
+    closesocket(s->udp_fd);
 #if HAVE_PTHREAD_CANCEL
     if (s->thread_started) {
         int ret;
@@ -1140,7 +1141,6 @@ static int udp_close(URLContext *h)
         pthread_cond_destroy(&s->cond);
     }
 #endif
-    closesocket(s->udp_fd);
     av_fifo_freep2(&s->fifo);
     ff_ip_reset_filters(&s->filters);
     return 0;
