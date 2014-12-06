cab48b2c7677c6c91e97b9dee66e6b07c6d2caf5
Hendrik Leppkes <h.leppkes@gmail.com>
2014-12-06 23:53:56 +0100

    mov: fix isom.h compat with c++

--- libavformat/isom.h
+++ libavformat/isom.h
@@ -311,7 +311,7 @@ typedef struct HEIFGrid {
 } HEIFGrid;
 
 typedef struct MOVContext {
-    const AVClass *class; ///< class for private options
+    const AVClass *avclass; ///< class for private options
     AVFormatContext *fc;
     int time_scale;
     int64_t duration;     ///< duration of the longest track
