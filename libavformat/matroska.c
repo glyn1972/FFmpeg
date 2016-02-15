797c7c7804d4727d2064ec1a05c0b6eff67b593c
Hendrik Leppkes <h.leppkes@gmail.com>
2016-02-15 11:43:57 +0100

    matroska: change values of the stereo mode tags to be more consistent

--- libavformat/matroska.c
+++ libavformat/matroska.c
@@ -130,9 +130,9 @@ const AVMetadataConv ff_mkv_metadata_conv[] = {
 
 const char * const ff_matroska_video_stereo_mode[MATROSKA_VIDEO_STEREOMODE_TYPE_NB] = {
     "mono",
-    "left_right",
-    "bottom_top",
-    "top_bottom",
+    "sbs_lr",
+    "tb_rl",
+    "tb_lr",
     "checkerboard_rl",
     "checkerboard_lr",
     "row_interleaved_rl",
@@ -140,8 +140,8 @@ const char * const ff_matroska_video_stereo_mode[MATROSKA_VIDEO_STEREOMODE_TYPE_
     "col_interleaved_rl",
     "col_interleaved_lr",
     "anaglyph_cyan_red",
-    "right_left",
+    "sbs_rl",
     "anaglyph_green_magenta",
-    "block_lr",
-    "block_rl",
+    "mvc_lr",
+    "mvc_rl",
 };
