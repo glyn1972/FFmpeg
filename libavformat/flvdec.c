8d170066fafdba0cfae3410fbfecce967eedd915
Hendrik Leppkes <h.leppkes@gmail.com>
2017-05-03 01:33:33 +0200

    flvdev: assume sizes to be broken if metadata header parsing failed

--- libavformat/flvdec.c
+++ libavformat/flvdec.c
@@ -1488,6 +1488,8 @@ retry:
                     av_log(s, AV_LOG_WARNING, "Adjusting next position due to index mismatch\n");
                     next = flv->validate_index[0].pos - 4;
                 }
+                if (type < 0)
+                    flv->broken_sizes = 1;
                 goto skip;
             } else if (type == TYPE_ONTEXTDATA) {
                 avpriv_request_sample(s, "OnTextData packet");
