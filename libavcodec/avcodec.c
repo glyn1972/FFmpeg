5b930b5f2b44b353abeb423edd91c5281be40387
Hendrik Leppkes <h.leppkes@gmail.com>
2021-06-16 08:24:35 +0200

    HACK: avcodec/utils: don't error out on missing channel info, the decoders are not fixed for this yet

--- libavcodec/avcodec.c
+++ libavcodec/avcodec.c
@@ -271,8 +271,10 @@ int attribute_align_arg avcodec_open2(AVCodecContext *avctx, const AVCodec *code
         && !(codec->capabilities & AV_CODEC_CAP_CHANNEL_CONF)) {
         av_log(avctx, AV_LOG_ERROR, "%s requires channel layout to be set\n",
                av_codec_is_decoder(codec) ? "Decoder" : "Encoder");
-        ret = AVERROR(EINVAL);
-        goto free_and_end;
+        if (!av_codec_is_decoder(codec)) {
+            ret = AVERROR(EINVAL);
+            goto free_and_end;
+        }
     }
     if (avctx->ch_layout.nb_channels && !av_channel_layout_check(&avctx->ch_layout)) {
         av_log(avctx, AV_LOG_ERROR, "Invalid channel layout\n");
