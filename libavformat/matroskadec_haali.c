f77d9669256103b8bf1fa513e214c0c8de33a36a
Hendrik Leppkes <h.leppkes@gmail.com>
2022-10-11 13:19:08 +0200

    matroskadec_haali: fix stream indexing in mkv_read_seek

--- libavformat/matroskadec_haali.c
+++ libavformat/matroskadec_haali.c
@@ -2214,7 +2214,7 @@ static int mkv_read_seek(AVFormatContext *s, int stream_index, int64_t timestamp
     cur_dts -= ctx->timeline[ctx->timeline_position].offset;
   }
 
-  avpriv_update_cur_dts(s, ctx->tracks[stream_index].stream, cur_dts);
+  avpriv_update_cur_dts(s, s->streams[stream_index], cur_dts);
 
   return 0;
 }
